ca5fbb73de1123bf7f8f5fb4fba9ecc990f97ae4
Oliver Poplawski <O.Poplawski2529@student.leedsbeckett.ac.uk>
2025-07-02 16:08:15 +0100

    pages-LoginActivityView Added Preview - Makes development and re-arrangement of composable elements easier. - Previous iteration was made with Kotlin playground, this will make it easier to implement changes within the code.

--- app/src/main/java/com/example/pp2025_reasses/pages/LoginActivityView.kt
+++ app/src/main/java/com/example/pp2025_reasses/pages/LoginActivityView.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -130,4 +131,11 @@ fun ConfirmButton(
         }
 
     }
+}
+
+@Preview
+@Composable
+fun LoginActivityPreview()
+{
+    LoginPage()
 }
