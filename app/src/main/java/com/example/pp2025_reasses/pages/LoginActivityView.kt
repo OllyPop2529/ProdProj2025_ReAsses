3d89f7a5fc396e977858b8e8477dff13984b4209
Oliver Poplawski <O.Poplawski2529@student.leedsbeckett.ac.uk>
2025-07-02 16:00:41 +0100

    pages-LoginActivityView 3 New Composable functions made - LoginPage, layout of the page with all contents (Currently Undecided)
    - PasswordInputField, is an input field that will allow the user to input a password.
      - No confirmation currently
      - No first time password setting yet.
      - Edited to start empty, but change to the password as it progresses. Will possible obscure password later.
    
    - ConfirmButton, will take the user's password and verify it.

--- app/src/main/java/com/example/pp2025_reasses/pages/LoginActivityView.kt
+++ app/src/main/java/com/example/pp2025_reasses/pages/LoginActivityView.kt
@@ -1,4 +1,85 @@
-package com.example.pp2025_reasses.pages
+package com.example.productionproject
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class LoginActivityView {
+
+}
+
+@Composable
+fun LoginPage()
+{
+
+}
+
+
+@Composable
+fun PasswordInputField()
+{
+    var data by remember { mutableStateOf("") }
+
+    TextField(
+        value =
+        if(data == "") ""
+        else data,
+        onValueChange = {data = it},
+        modifier = Modifier
+            .padding(vertical = 10.dp, horizontal = 5.dp)
+    )
+}
+
+
+@Composable
+fun ConfirmButton(
+    onClick: () -> Unit,
+    modifier: Modifier,
+    text : String
+) {
+    Button(
+        onClick = { onClick() },
+        shape = RoundedCornerShape(corner = CornerSize(10.dp)),
+        modifier = modifier,
+        elevation = ButtonDefaults.buttonElevation(5.dp,0.dp)
+    )
+    {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxSize()
+        )
+        {
+            Text(
+                text = text,
+                textAlign = TextAlign.Center,
+                fontSize = 22.sp,
+                lineHeight = 20.sp,
+                modifier = Modifier
+                    .weight(1.5f)
+                    .padding(5.dp)
+
+            )
+
+        }
+
+    }
 }
