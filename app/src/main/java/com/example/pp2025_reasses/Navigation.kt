1f2e3df90297b9c87d528ad14ba2f9ffc4eb5beb
Oliver Poplawski <O.Poplawski2529@student.leedsbeckett.ac.uk>
2025-07-04 15:16:00 +0100

    main-Navigation: New Kotlin File: Navigation.kt - Responsible for navigation between the three main pages of the application. - Uses a NavHostController to move between pages when interacted with correctly. - Navigation currently not implemented. Individual work on each page has to be implemented for this to work.

---
+++ app/src/main/java/com/example/pp2025_reasses/Navigation.kt
@@ -0,0 +1,51 @@
+package com.example.pp2025_reasses
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pp2025_reasses.pages.LocationPage
+import com.example.pp2025_reasses.pages.SettingsPage
+import com.example.productionproject.LoginPage
+
+
+    @Composable
+    fun AppNavigation(
+        //viewModel: OrderViewModel = viewModel(),
+        navController: NavHostController = rememberNavController()
+    )
+    {
+        //val uiState by viewModel.uiState.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = LocationTrackingApp.Login.name,
+            modifier = Modifier.padding(all = 10.dp)
+        ) {
+
+            composable(route = LocationTrackingApp.Login.name) {
+                LoginPage()
+
+            }
+
+            composable(route = LocationTrackingApp.Location.name) {
+                LocationPage()
+
+            }
+            composable(route = LocationTrackingApp.Setting.name) {
+                SettingsPage()
+
+            }
+
+        }
+    }
+
+    enum class LocationTrackingApp()
+    {
+        Login,
+        Location,
+        Setting
+    }
