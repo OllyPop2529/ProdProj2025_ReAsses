bd62e92fd16ba92ba295381e78765fb2b5a1a657
Oliver Poplawski <O.Poplawski2529@student.leedsbeckett.ac.uk>
2025-07-04 13:53:20 +0100

    pages-SettingActivityView-OptionBlock Modifications to aspect proportionality - Is now stackable and correctly formatted - (Future) Will take formatting from theme (e.g TextStyle + Font)
    pages-SettingActivityView-SettingInputField
    Added padding to center inputefield
    - Further styling may be done

--- app/src/main/java/com/example/pp2025_reasses/pages/SettingsActivityView.kt
+++ app/src/main/java/com/example/pp2025_reasses/pages/SettingsActivityView.kt
@@ -1,8 +1,12 @@
 package com.example.pp2025_reasses.pages
 
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -11,9 +15,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import java.lang.reflect.Modifier
+import androidx.compose.ui.unit.dp
 
 class SettingsActivityView {
 
@@ -46,7 +52,6 @@ class SettingsActivityView {
         description: String,
         type: Int,
         condition: String?,
-        modifier: androidx.compose.ui.Modifier
     )
     {
         Surface()
@@ -55,33 +60,43 @@ class SettingsActivityView {
 
             //Section Block
             Row(
-
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp),
+                verticalAlignment = Alignment.CenterVertically
             )
             {
                 // Description Section
-                Column {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(0.7f)
+                )
+                {
+                    //Title of Option
                     Text(
                         text = descriptionName,
-                        textAlign = TextAlign.Center,
-                        modifier = androidx.compose.ui.Modifier
-                            .weight(0.15f)
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier
+
                     )
 
+                    //Description of Option
                     Text(
                         text = description,
                         textAlign = TextAlign.Start,
-                        modifier = androidx.compose.ui.Modifier
-                            .weight(0.85f)
+                        modifier = Modifier
+
                     )
                 }
 
-
+                val modifier: Modifier = Modifier.weight(0.3f)
                 //Setting Input Type
                 when (type) {
                     //Input Box
                     1 ->
                     {
-                        SettingInputField(condition)
+                        SettingInputField(condition, modifier)
                     }
                 }
             }
@@ -91,7 +106,7 @@ class SettingsActivityView {
     }
 
     @Composable
-    fun SettingInputField(condition: String?)
+    fun SettingInputField(condition: String?, modifier: Modifier)
     {
         var data by remember { mutableStateOf("") }
 
@@ -104,7 +119,9 @@ class SettingsActivityView {
                     condition
                 else
                     ""
-            }
+            },
+            modifier = modifier
+                .padding(top = 10.dp, start = 5.dp, end = 5.dp, bottom = 10.dp)
         )
     }
 
