521392b22f98e457595c950be3d48d267af87f8b
Oliver Poplawski <O.Poplawski2529@student.leedsbeckett.ac.uk>
2025-07-04 13:48:05 +0100

    pages-SettingActivityView-OptionBlock Contains the setting option - Has description + description name - (WIP) Types of Settings    - Will select appropriate interaction type based on setting predisposition    - Button, Toggle, Input Field, DropDown
    pages-SettingActivityView-SettingInputField
    Allows for user string input
    - Allows for user to input a value
    - Does not currently save/change value elsewhere.

--- app/src/main/java/com/example/pp2025_reasses/pages/SettingsActivityView.kt
+++ app/src/main/java/com/example/pp2025_reasses/pages/SettingsActivityView.kt
@@ -1,8 +1,17 @@
 package com.example.pp2025_reasses.pages
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import java.lang.reflect.Modifier
 
@@ -32,12 +41,72 @@ class SettingsActivityView {
     }
 
     @Composable
-    fun OptionBlock(description: String, modifier: Modifier)
+    fun OptionBlock(
+        descriptionName: String,
+        description: String,
+        type: Int,
+        condition: String?,
+        modifier: androidx.compose.ui.Modifier
+    )
     {
+        Surface()
+        {
 
+
+            //Section Block
+            Row(
+
+            )
+            {
+                // Description Section
+                Column {
+                    Text(
+                        text = descriptionName,
+                        textAlign = TextAlign.Center,
+                        modifier = androidx.compose.ui.Modifier
+                            .weight(0.15f)
+                    )
+
+                    Text(
+                        text = description,
+                        textAlign = TextAlign.Start,
+                        modifier = androidx.compose.ui.Modifier
+                            .weight(0.85f)
+                    )
+                }
+
+
+                //Setting Input Type
+                when (type) {
+                    //Input Box
+                    1 ->
+                    {
+                        SettingInputField(condition)
+                    }
+                }
+            }
+
+            ////////////////////////////////////////////////////////
+        }
     }
 
+    @Composable
+    fun SettingInputField(condition: String?)
+    {
+        var data by remember { mutableStateOf("") }
 
+        TextField(
+            value = data,
+            onValueChange = {data = it},
+            label =
+            {
+                if (condition != null)
+                    condition
+                else
+                    ""
+            }
+        )
+    }
 
 
 
