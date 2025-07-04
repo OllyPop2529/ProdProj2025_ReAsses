c8df7e192d912c0e1dd472ee5b5fcf4625035a93
Oliver Poplawski <O.Poplawski2529@student.leedsbeckett.ac.uk>
2025-07-04 13:44:24 +0100

    pages-SettingActivityView: Created 6 New Functions - SettingPagePreview, A preview for the entire activity - DropDownPreview, A preview for a single expandable drop down menu - SectionHeader, The title for the options (i,e Bluetooth), will also be the way to expand the menu. - OptionBlock, A singular setting, placed inside of a block so they can be stacked. - SectionBlock, will be the combined result of the SectionHeader and OptionBlocks - SettingPage, will be the combined display of all section blocks

---
+++ app/src/main/java/com/example/pp2025_reasses/pages/SettingsActivityView.kt
@@ -0,0 +1,67 @@
+package com.example.pp2025_reasses.pages
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import java.lang.reflect.Modifier
+
+class SettingsActivityView {
+
+
+
+    @Composable
+    fun SettingPage()
+    {
+
+    }
+
+
+    @Composable
+    fun SectionBlock()
+    {
+
+    }
+
+
+
+    @Composable
+    fun SectionHeader(title: String, modifier: Modifier)
+    {
+
+    }
+
+    @Composable
+    fun OptionBlock(description: String, modifier: Modifier)
+    {
+
+    }
+
+
+
+
+
+
+
+    @Preview(showBackground = true)
+    @Composable
+    private fun SettingPagePreview()
+    {
+
+
+
+    }
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DropDownPreview()
+    {
+        SectionHeader(
+            "ABC",
+            modifier = Modifier
+        )
+
+    }
+
+}
