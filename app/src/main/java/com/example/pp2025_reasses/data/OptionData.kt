34429969cf8e1e6bbc7fb6e7baf7812d6055ead9
Oliver Poplawski <O.Poplawski2529@student.leedsbeckett.ac.uk>
2025-07-06 13:53:46 +0100

    data-OptionData New Data Class: OptionData - Uses OptFeature class to store information about Theme and Google maps settings
    res-strings
    Added Strings for Theme settings and Google maps settings (WIP)

---
+++ app/src/main/java/com/example/pp2025_reasses/data/OptionData.kt
@@ -0,0 +1,24 @@
+package com.example.pp2025_reasses.data
+
+import com.example.pp2025_reasses.R
+import com.example.pp2025_reasses.model.OptFeature
+
+class OptionData() {
+    fun loadGMaps(): List<OptFeature> {
+        return listOf<OptFeature>(
+            OptFeature(R.string.gmT1, R.string.gmD1, 3),
+            OptFeature(R.string.gmT2, R.string.gmD2, 1),
+
+
+            )
+    }
+
+    fun loadTheme(): List<OptFeature> {
+        return listOf<OptFeature>(
+            OptFeature(R.string.themeT1, R.string.themeD1, 1),
+            OptFeature(R.string.themeT2, R.string.themeD2, 1),
+            OptFeature(R.string.themeT3, R.string.themeD3, 1),
+
+            )
+    }
+}
